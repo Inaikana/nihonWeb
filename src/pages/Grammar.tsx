0585ab5ee846aea80a0db09d734e8d748f7dd96b
Inai <aromr990426@gmail.com>
2026-03-10 22:50:20 +0800

    style: 筆記UI製作

--- src/pages/Grammar.tsx
+++ src/pages/Grammar.tsx
@@ -1,8 +1,9 @@
 import { MainLayout } from "../layouts/MainLayout";
 import { grammarsData } from "../FakeData/fakedata";
 import { Hiragana } from "../components/Hiragana";
-import { LuNotebookText } from "react-icons/lu";
 import { FaPencilAlt } from "react-icons/fa";
+import { LuNotebookText } from "react-icons/lu";
+import { FaRegFlag } from "react-icons/fa6";
 
 const id = "0101"; // 篩選集數 這邊先寫定
 const filteredGrammar = grammarsData.filter((grammar) => {
@@ -13,45 +14,62 @@ const grammarData = filteredGrammar[0];
 export function Grammar() {
   return (
     <MainLayout className="flex flex-col items-center w-full bg-slightWhile">
-      <div className="w-[60%] bg-white shadow-md  px-4">
+      <div className="w-[60%] bg-white shadow-md  px-12">
         {/* 【 1 】 集數 */}
         <p className="bg-main text-[20px] mt-6 px-2 py-1 rounded-md inline-block">
           第{grammarData.episodeNumber}集
         </p>
-        {/* <h2 className="text-[32px] font-bold text-black mt-4">
-          {grammarData.grammarSummary}
-        </h2> */}
-        {/* <h3 className="flex items-center">
-          <FaPencilAlt />
-          <p className="ml-2">文法公式</p>
-        </h3> */}
 
         {/* 【 2 】 文法公式 */}
-        <h2 className="bg-softBlue mt-8 px-3 py-2 font-bold text-[24px] rounded-lg">
-          {grammarData.grammarPattern}
-        </h2>
-        <p className="text-[20px] mt-2">{grammarData.chineseMeaning}</p>
+
+        <div className="mt-8">
+          <h3 className="flex items-center text-[16px]">
+            <FaPencilAlt />
+            <p className="ml-2">公式</p>
+          </h3>
+
+          <h2 className="bg-softBlue mt-2 px-3 py-2 font-bold text-[24px] rounded-lg">
+            {grammarData.grammarPattern}
+          </h2>
+          <p className="text-[20px] mt-2">{grammarData.chineseMeaning}</p>
+        </div>
 
         {/* 【 3 】 備註 */}
-        <div className="mt-6">
-          <h3 className="flex items-center text-[20px]">
+        <div className="mt-10">
+          <h3 className="flex items-center text-[16px]">
             <LuNotebookText />
             <p className="ml-2">備註</p>
           </h3>
-
-          {grammarData.notes.map((note) => (
-            <div>{note}</div>
-          ))}
+          <div className="bg-softPink border-2 border-sub rounded-xl mt-2 py-3 px-4">
+            {grammarData.notes.map((note) => (
+              <div>{note}</div>
+            ))}
+          </div>
         </div>
 
         {/* 【 4 】 例句 */}
-        {grammarData.examples.map((example, index) => (
-          <p className="text-[px]" key={index}>
-            <Hiragana text={example.japanese}></Hiragana>
-          </p>
-        ))}
+        <div className="mt-10">
+          <h3 className="flex items-center text-[16px]">
+            <FaRegFlag />
+            <p className="ml-2">例句</p>
+          </h3>
 
-        {/* <p>{grammarData.}</p> */}
+          <div className="mt-2 text-[20px]">
+            {grammarData.examples.map((example, index) => (
+              <p key={index}>
+                <Hiragana text={example.japanese}></Hiragana>
+              </p>
+            ))}
+          </div>
+        </div>
+
+        {/* 【 5 】 參考影片 */}
+        <div className="mt-10">
+          <h3 className="flex items-center text-[16px]">
+            <FaRegFlag />
+            <p className="ml-2">參考影片</p>
+          </h3>
+        </div>
       </div>
     </MainLayout>
   );
